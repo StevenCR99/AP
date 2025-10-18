d518627a0c02b9661a18b9389f8e41f29b281638
steven mata <smata9903@gmail.com>
2025-10-18 10:44:42 -0600

    Clase y constructor persona

--- AP/Program.cs
+++ AP/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgendaPro
+{
+    class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public Persona(int id, string nombre, string telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Telefono = telefono;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Nombre} - {Telefono}";
+        }
+    }
